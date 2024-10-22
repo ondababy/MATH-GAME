ff5d90be25a43cff310b0ce8a2a64cc41ec67e17
OndaBaby <139729721+OndaBaby@users.noreply.github.com>
2024-10-22 21:11:36 +0800

    ehehe

--- app/includes/header.php
+++ app/includes/header.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="resources/css/style.css" rel="stylesheet" type="text/css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-    <title>shop </title>
+    <title>Do Math & Conquer</title>
 </head>
 <body>
 <nav class="navbar navbar-light bg-light justify-content-between">
